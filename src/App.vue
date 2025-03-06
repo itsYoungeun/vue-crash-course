5a5d6a88b40f8a5bf6d73d72df8b695e3bf94953
Peter <youngeunpc@yahoo.com>
2025-03-06 20:01:55 -0300

    Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<!-- Vue 3 Composition API -->
-
 <script setup>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
 
 <template>
   <Navbar />
+  <Hero title="Test Title" subtitle="Test Subtitle"/>
 </template>
