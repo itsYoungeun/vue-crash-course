8029804d24ab53095423319a8d5143a71d9bc0eb
Peter <youngeunpc@yahoo.com>
2025-03-07 20:57:49 -0300

    Implemented back button and proxy for localhost

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig({
   ],
   server: {
     port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
   },
   resolve: {
     alias: {
